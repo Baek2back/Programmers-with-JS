52d21eecdc5cce3115729be81a490ff97d4d90e0
Baek2back <dumbuk12@naver.com>
2020-12-13 20:04:28 +0900

    2020-12-13

--- Level-3/불량 사용자.js
+++ Level-3/불량 사용자.js
@@ -11,28 +11,34 @@ function solution(user_id, banned_id) {
       return (result = [...result, ...attached]);
     }, []);
   };
-  const bannedId = banned_id.map(id => id.replace(/\*/g, '.'));
+  banned_id = banned_id.map(id => id.replace(/\*/g, '.'));
   const candidates = getPermutations(user_id, banned_id.length);
   let answer = candidates.filter(candidate => {
-    let result = [...candidate];
-    bannedId.forEach(banId => {
-      const regExp = new RegExp(`${banId}`);
+    let current = [...candidate];
+    banned_id.forEach(regExp => {
+      const bannedRegExp = new RegExp(`${regExp}`);
       let idx = 0;
-      for (const [id, status] of result) {
-        if (status && regExp.test(id)) {
-          result[idx] = [id, false];
+      for (const [id, status] of current) {
+        if (status && regExp.length === id.length && bannedRegExp.test(id)) {
+          current[idx] = [id, false];
           break;
         }
         idx++;
       }
     });
-    return !result.filter(([_, status]) => status).length;
+    return !current.filter(([_, status]) => status).length;
   });
-  answer = answer.map(row =>
-    row.map(v => {
-      const [id] = v;
-      return id;
+  answer = answer
+    .map(row =>
+      row.map(v => {
+        const [id] = v;
+        return id;
+      })
+    )
+    .map(row => {
+      const sortedRow = [...row];
+      return sortedRow.sort((a, b) => (a < b ? -1 : 1));
     })
-  );
-  return answer;
+    .map(v => v.join(' '));
+  return new Set(answer).size;
 }
